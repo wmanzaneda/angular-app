b81d7bad07544f12eca5782bb85fdadc18898b16
wmanzaneda <wmanzaneda@gmail.com>
2019-03-19 09:43:15 -0400

    4 files modified, 1 added

--- src/app/components/body.component.ts
+++ src/app/components/body.component.ts
@@ -2,7 +2,7 @@ import { Component} from '@angular/core';
 
 @Component({
   selector:'body-component',
-  template:'./body.component.html'
+  templateUrl:'./body.component.html'
 })
 export class BodyComponent{
   
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,9 +2,9 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `  <h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  templateUrl:'./hello.component.html'
+  
 })
 export class HelloComponent  {
-  @Input() name: string;
+  
 }
